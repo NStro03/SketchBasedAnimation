3d91a1a2a4cb949feb9d0ca8b2091d37c92fbc30
anil <anilkumar8645921@gmail.com>
2020-11-24 17:45:40 +0530

    src/utils.h files updated

--- src/utils.h
+++ src/utils.h
@@ -8,6 +8,7 @@
 #include <glm/vec3.hpp> // glm::vec3
 #include <glm/vec4.hpp> // glm::vec4
 #include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/mat3x3.hpp> // glm::mat3
 #include <glm/gtc/matrix_transform.hpp> 
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
